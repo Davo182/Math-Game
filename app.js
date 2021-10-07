d6e217d95b1070ca644ad7a02da00ccccb0e8ae5
Davo182 <davidanachuri@gmail.com>
2021-10-07 19:46:37 -0300

    Final

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ let starTime = function () {
                 time = 10;
                 $('#answer-input').val('');
                 renderTime();
-                maxScore = score;
+                if(score > maxScore) maxScore = score;
                 score = 0;
                 $('#score').text(score);
                 $('#high-score').text(maxScore);
@@ -28,34 +28,61 @@ let starTime = function () {
 }
 
 let createQuestion = function (limit) {
-    return [Math.floor(Math.random() * limit), Math.floor(Math.random() * limit)];
+    let typeArr = [];
+    $('input[type=checkbox]').each(function () {
+        if ($(this).is(':checked')) typeArr.push($(this).siblings().first().text());
+    });
+    let rand = Math.floor(Math.random() * typeArr.length);
+    return { values: [Math.floor(Math.random() * limit) + 1, Math.floor(Math.random() * limit) + 1], type: typeArr[rand] }
+
 }
 let question = createQuestion($('#question-range').val());
 
-$('#question').text(`${question[0]} + ${question[1]}`);
+$('#question').text(`${question.values[0]} + ${question.values[1]}`);
 $('#answer-input').on('keyup', function () {
     starTime();
     let answerInput = $('#answer-input').val();
-    compareAnwser(question.reduce((a, b) => a + b), parseInt(answerInput));
+    compareAnwser(parseInt(answerInput));
 });
 
 $('#question-range').on('change', function () {
     $('#range-text').text($(this).val());
 })
 
-let compareAnwser = function (qs, answer) {
+let compareAnwser = function (answer) {
+    let qs = createAnswer();
     if (qs === answer) {
         time++;
         renderTime();
-        score++;    
+        score++;
         $('#score').text(score);
         question = createQuestion($('#question-range').val());
         renderQuestion();
     }
 }
 
+let createAnswer = function () {
+    switch (question.type) {
+        case '+':
+            return question.values.reduce((a, b) => a + b);
+        case '-':
+            return question.values.reduce((a, b) => a - b);
+        case 'x':
+            return question.values.reduce((a, b) => a * b);
+        case '/':
+            return Math.floor(question.values.reduce((a, b) => b / a));
+    }
+}
+
 let renderQuestion = function () {
-    question = createQuestion($('#question-range').val());
-    $('#question').text(`${question[0]} + ${question[1]}`);
+    let range = $('#question-range').val();
+    question = createQuestion(range);
+    if(question.type === '/') {
+        question.values[0] = (Math.floor(Math.random() * range) + 1);
+        question.values[1] = question.values[0] * (Math.floor(Math.random() * range) + 1);
+        $('#question').text(`${question.values[1]} ${question.type} ${question.values[0]}`);
+    } else {
+        $('#question').text(`${question.values[0]} ${question.type} ${question.values[1]}`);
+    }
     $('#answer-input').val('');
 }
